4ba9e5474df97f645eacf5aa815a0a05a0c75a06
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-06-05 02:35:48 -0500

    Hrm

--- JotunnModStub/MonsterLoader.cs
+++ JotunnModStub/MonsterLoader.cs
@@ -12,18 +12,20 @@ using Jotunn.Managers;
 using Jotunn.Entities;
 using System;
 using Jotunn.Configs;
+using System.Collections.Generic;
 
 namespace MonsterLoader
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Major)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.None)]
     internal class MonsterLoader : BaseUnityPlugin
     {
         public const string PluginGUID = "com.zarboz.MonsterLoader";
         public const string PluginName = "MonsterLoader";
         public const string PluginVersion = "0.0.1";
         private AssetBundle assetBundle;
+        private AssetBundle yetiboy;
 
         private void Awake()
         {
@@ -41,19 +43,47 @@ namespace MonsterLoader
         private void loadassets()
         {
             assetBundle = AssetUtils.LoadAssetBundleFromResources("earthtroll", typeof(MonsterLoader).Assembly);
+            yetiboy = AssetUtils.LoadAssetBundleFromResources("yetiboy", typeof(MonsterLoader).Assembly);
         }
 
          
         private void loadenemy()
         {
             try { 
+
             var fab = assetBundle.LoadAsset<GameObject>("EarthTroll");
             var humanoid = fab.GetComponent<Humanoid>();
-            humanoid.m_health = 1500;
+            var fuckstain = PrefabManager.Cache.GetPrefab<GameObject>("Coins");
+            List<CharacterDrop.Drop> fuckingshit = new List<CharacterDrop.Drop>
+            {
+                new CharacterDrop.Drop
+                {
+                    m_prefab = fuckstain,
+                }
+            };
+                humanoid.m_health = 1500;
 
-            var fab2 = assetBundle.LoadAsset<GameObject>("CrazyTroll");
+                var fuckingshitballs = fab.AddComponent<CharacterDrop>();
+                fuckingshitballs.m_drops = fuckingshit;
+                fuckingshitballs.m_dropsEnabled = true;
+                var fab2 = assetBundle.LoadAsset<GameObject>("CrazyTroll");
+                var eatdickbro = fab2.AddComponent<CharacterDrop>();
+                eatdickbro.m_drops = fuckingshit;
+                eatdickbro.m_dropsEnabled = true;
+
+                var stupidyeti = yetiboy.LoadAsset<GameObject>("YetiBoy");
+                var stupidfuck = stupidyeti.AddComponent<CharacterDrop>();
+                stupidfuck.m_drops = fuckingshit;
+                stupidfuck.m_dropsEnabled = true;
+                var YetiboyFenring = yetiboy.LoadAsset<GameObject>("YetiboyFenring");
+                var YetiboyFenring2 = stupidyeti.AddComponent<CharacterDrop>();
+                YetiboyFenring2.m_drops = fuckingshit;
+                YetiboyFenring2.m_dropsEnabled = true;
+                Jotunn.Logger.LogMessage("Character drops added....for RRR I guess?");
             PrefabManager.Instance.AddPrefab(fab2);
             PrefabManager.Instance.AddPrefab(fab);
+            PrefabManager.Instance.AddPrefab(stupidyeti);
+            PrefabManager.Instance.AddPrefab(YetiboyFenring);
             }
             catch (Exception ex)
             {
